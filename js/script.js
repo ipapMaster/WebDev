b52962e0ed1d9fd4dd03522223bf8028fb721108
mrharut <m@l.ru>
2023-11-10 20:12:37 +0300

    LessCompile

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".hamburger-menu").addEventListener("click", ()=>{
+    document.querySelector(".nav-links").classList.toggle("show-menu");
+});
+
+document.querySelector(".nav-links").addEventListener("click", ()=>{
+    document.querySelector(".nav-links").classList.remove("show-menu");
+});
